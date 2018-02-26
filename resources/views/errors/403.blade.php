b03e1ae5607f6ecb30bf29501cedb9bf4d16f733
Tadas Varanauskas <tadas@varanauskas.lt>
2018-02-26 11:48:19 +0200

    Updated 403 error page styling

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -6,6 +6,7 @@
 @section('title', 'Login')
 
 @section('content')
-    <h1>{{ $exception->getMessage() }}</h1>
+    <h1>Error 403</h1>
+    <h2>{{ $exception->getMessage() }}</h2>
     <a href="{{ url()->previous() }}">Go back</a>
 @endsection
